887c2027b2df9d4eec67be2534e6ed1b46eb77c0
25LP5951 25LP5951 <gna.k6911@gmail.com>
2025-12-22 15:42:07 +0800

    xcmndihcid

---
+++ movie/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="max-w-4xl p-8">
+        <h1 className="text-3xl font-bold mb-4">About Us</h1>
+        <p className="text-lg text-gray-700 dark:text-gray-300">
+          Welcome to our movie database application. We are dedicated to
+          providing the best movie experience for our users.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ movie/src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <div className="flex w-vw h-15 bg-white justify-center items-center pl-30 pr-30">
+      <div className="flex flex-row justify-between w-dvw ">
+        <button>
+          <img src="Logo.png" alt="" />
+        </button>
+        <input className="flex pl-5 border-gray-400 border rounded-[10]" type="search" placeholder=" Search... " />
+        <button>
+          <img src="darkTheme.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
